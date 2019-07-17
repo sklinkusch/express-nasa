1130ce380ac8fc4bda29aeb14ced9ad1f7df0d51
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-17 14:59:46 +0200

    use version and help functions

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const { doAPOD, doAsteroids } = require("./request");
+const { showVersion, showHelp } = require("./messaging");
+
+const commandlineArgs = process.argv.slice(2);
+if (commandlineArgs.includes("--version")) {
+  showVersion();
+  process.exit();
+}
+if (commandlineArgs.includes("--help")) {
+  showHelp();
+  process.exit();
+}
 
 app.get('/apod', (req, res) => doAPOD(req, res));
 app.get('/asteroids', (req, res) => doAsteroids(req, res));
