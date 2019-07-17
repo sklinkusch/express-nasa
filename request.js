e8d56a606c92dd3bd9cee193ee8687a82f1f75d2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-17 11:00:36 +0200

    finish birthday request

--- request.js
+++ request.js
@@ -3,15 +3,28 @@ const axios = require('axios');
 const api_key = "ToMkX8ppOaHwLbx4nhHegwzkGi6HUo3BhhMAV8A1";
 exports.doRequest = async (req, res) => {
   let apiUrl;
-  if ("date" in req.query) {
-    const { date: sampleDate } = req.query;
-    apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${sampleDate}`;
+  const { date: sampleDate } = req.query;
+  const [selectedMonth, selectedDay] = sampleDate.split("-");
+  const today = new Date()
+  const [currentMonth, currentDay, currentYear] = today.toLocaleDateString('en-GB').split("/")
+  let minimum, maximum;
+  if (currentMonth >= selectedMonth && currentDay >= selectedDay) {
+    minimum = 0;
+    maximum = 5;
   } else {
-    apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${api_key}`;
+    minimum = 1;
+    maximum = 6;
   }
-  const response = await axios.get(apiUrl);
-  const { headers, data } = await response;
-  const { 'x-ratelimit-remaining': remainingRequests } = await headers;
-  const { title, date, explanation, url } = data;
-  return res.json({ remainingRequests, date, title, explanation, url });
+  const apods = [];
+  for (let i = minimum; i < maximum; i++) {
+    year = currentYear - i;
+    const requestDate = `${year}-${selectedMonth}-${selectedDay}`
+    apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${requestDate}`;
+    const response = await axios.get(apiUrl);
+    const { headers, data } = await response;
+    const { 'x-ratelimit-remaining': remainingRequests } = await headers;
+    const { title, date, explanation, url } = data;
+    apods.push({ remainingRequests, date, title, explanation, url });
+  }
+  return res.json(apods);
 }
