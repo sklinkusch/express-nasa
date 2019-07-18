3ee9163f9a0a611c4d38886e6e3354aa1237b773
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-18 11:53:33 +0200

    add term mode

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000;
 const { doAPOD, doAsteroids } = require("./request");
 const { showVersion, showHelp } = require("./messaging");
 const { dateInInput, checkDateFormat, wrongEndpoint } = require("./errors");
+const { basicPrompt } = require("./prompt");
 
 const commandlineArgs = process.argv.slice(2);
 if (commandlineArgs.includes("--version")) {
@@ -14,10 +15,14 @@ if (commandlineArgs.includes("--help")) {
   showHelp();
   process.exit();
 }
-app.use('/apod', (req, res, next) => dateInInput(req, res, next));
-app.use('/apod', (req, res, next) => checkDateFormat(req, res, next));
-app.get('/apod', (req, res) => doAPOD(req, res));
-app.get('/asteroids', (req, res) => doAsteroids(req, res));
-app.all('*', (req, res) => wrongEndpoint(req, res));
+if (commandlineArgs.includes("--term")) {
+  basicPrompt();
+} else {
+  app.use('/apod', (req, res, next) => dateInInput(req, res, next));
+  app.use('/apod', (req, res, next) => checkDateFormat(req, res, next));
+  app.get('/apod', (req, res) => doAPOD(req, res));
+  app.get('/asteroids', (req, res) => doAsteroids(req, res));
+  app.all('*', (req, res) => wrongEndpoint(req, res));
 
-app.listen(port);
+  app.listen(port);
+}
